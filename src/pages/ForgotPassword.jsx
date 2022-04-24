2c2b9ba1135be22f57b9c3ec976b96b399c3f9df
Jones Navela <navelajones@gmail.com>
2022-04-24 09:13:53 +0200

    Pages & Routes

---
+++ src/pages/ForgotPassword.jsx
@@ -0,0 +1,10 @@
+
+function ForgotPassword() {
+  return (
+    <div>
+      <h1>ForgotPassword page</h1>
+    </div>
+  )
+}
+
+export default ForgotPassword
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,10 @@
+
+function SignUp() {
+  return (
+    <div>
+      <h1>SignUp page</h1>
+    </div>
+  )
+}
+
+export default SignUp
